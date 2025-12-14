3e739eff066bb1d3991fb93623af33b8c49d458e
Karam Ghanem <karamghanem34@gmail.com>
2025-12-14 14:53:31 +0300

    Install TailwindCSS

--- src/components/ColorModeToggle.tsx
+++ src/components/ColorModeToggle.tsx
@@ -1,5 +1,5 @@
 import { Button, } from "@chakra-ui/react";
-import { useColorMode } from "./color-mode";
+import { useColorMode } from "@/components/ui/color-mode";
 
 function ColorModeToggle() {
   const { colorMode, toggleColorMode } = useColorMode();
